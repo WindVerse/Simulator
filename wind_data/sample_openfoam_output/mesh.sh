c85221675816464cb4fb5575aa603b5057a6ac25
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2026-05-12 23:22:31 +0530

    Added sample openfoam output

---
+++ wind_data/sample_openfoam_output/mesh.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c480a130faea15b910d11936cad19962208c8ace909c086832e39229e206a3d
+size 652
---
+++ wind_data/sample_openfoam_output/yPlus.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e72bd33cd76ecc3fa2dc06378daaedf827cbabee6a6509d34a2039ded813b10
+size 562
